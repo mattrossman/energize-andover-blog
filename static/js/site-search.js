2f4972705672e6b41a14d3b8dc1efe7c2d88da2e
mattrossman <mrossman@umass.edu>
2017-07-22 16:51:32 -0400

    Added lunr.js and site search script

---
+++ static/js/site-search.js
@@ -0,0 +1,33 @@
+var siteSearch;
+
+$.getJSON('/energize-andover-blog/index.json',jsonCallback);
+
+
+function jsonCallback(data) {
+	var idx = lunr(function () {
+		this.field('content')
+		this.ref('title')
+
+		data.forEach(function (doc) {
+			this.add(doc)
+		}, this)
+	});
+	
+	siteSearch = function(query) {
+		var search_result = idx.search(query);
+		var getRef = function(obj){ return obj.ref; };
+		console.log(search_result.map(getRef))
+	}
+};
+
+
+function submitSearch(){
+	query = document.getElementById('site-search').value;
+	siteSearch(query);
+}
+
+document.onkeydown=function(e){
+    if(e.keyCode=='13'){
+        submitSearch();
+    }
+}
