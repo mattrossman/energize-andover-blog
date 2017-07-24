321c807bee2c5f68146b2aeeba1461e080981db4
mattrossman <mrossman@umass.edu>
2017-07-23 20:52:05 -0400

    Removed posts with empty description from site index (to remove non-hidden blank entries)

--- docs/js/site-search.js
+++ docs/js/site-search.js
@@ -49,10 +49,8 @@ document.onkeydown=function(e){
 }
 
 function displayPosts(names) {
-	for (var i=0; i<names.length; ++i) {
-		if (names[i])
-			document.getElementById(names[i]).style.display = 'block';
-	}
+	for (var i=0; i<names.length; ++i)
+		document.getElementById(names[i]).style.display = 'block';
 }
 
 
