b3783eff7cf053cbe80715cdbd834d922092df8d
mattrossman <mrossman@umass.edu>
2019-03-19 22:53:34 +0100

    rename btn-featured id

--- static/js/featured-posts.js
+++ static/js/featured-posts.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-    let btnFeaturedPosts = $("#btnFeaturedPosts");
+    let btnFeaturedPosts = $("#btn-featured");
 
     // toggles showing only featured posts or all posts
     function toggleFeatured() {
         $("#post-list").children().toggleClass("featured-filter");
         $("#post-list-status").toggleClass("featured-filter");
-        onlyFeatured = !onlyFeatured;
     }
     
     // configure btn handler
