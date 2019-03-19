a46a0a3579c404e8de136f5edb883df2791ad483
mattrossman <mrossman@umass.edu>
2019-03-19 22:50:21 +0100

    move most post list filter logic to css

--- static/js/featured-posts.js
+++ static/js/featured-posts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    var onlyFeatured = false;
     let btnFeaturedPosts = $("#btnFeaturedPosts");
 
     // toggles showing only featured posts or all posts
     function toggleFeatured() {
-        $(".not-featured").toggle();
+        $("#post-list").children().toggleClass("featured-filter");
+        $("#post-list-status").toggleClass("featured-filter");
         onlyFeatured = !onlyFeatured;
-        $("#post-list-status").text(onlyFeatured ? "only featured posts" : "all posts");
     }
     
     // configure btn handler
