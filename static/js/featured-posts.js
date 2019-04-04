b6974790a2a8ab90b065a4b4b8dfd024df72f056
mattrossman <mrossman@umass.edu>
2019-04-04 14:27:13 +0200

    add hash to URL to preserve filter state
    This way if the user goes back to the home page the list will still be
    sorted/unsorted depending on the previous state

--- static/js/featured-posts.js
+++ static/js/featured-posts.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
     let btnFeaturedPosts = $("#btn-featured");
+    let hashFeatured = "#featured";
+    let hashAll = "#all";
 
     // toggles showing only featured posts or all posts
-    function toggleFeatured() {
-        $("#post-list").children().toggleClass("featured-filter");
-        $("#post-list-status").toggleClass("featured-filter");
+    function toggleFeatured(state = null) {
+        $("#post-list").children().toggleClass("featured-filter", state);
+        $("#post-list-status").toggleClass("featured-filter", state);
+
+        if ($("#post-list-status").hasClass("featured-filter")) {
+            setUrlHash(hashFeatured);
+        }
+        else {
+            setUrlHash(hashAll);
+        }
     }
-    
+
+    function parseHash(hash) {
+        if (hash == hashFeatured) { toggleFeatured(true); }
+        else if (hash == hashAll) { toggleFeatured(false); }
+    }
+
+    function setUrlHash(hash) {
+        window.location.replace(window.location.origin + hash);
+    }
+
     // configure btn handler
     btnFeaturedPosts.click(toggleFeatured);
+
+    // check if the user is reloading a page that should be filtered
+    parseHash(window.location.hash);
 });
